08c34dc8373ffaebdfbe3ff74cccf04c4d7fa57c
Njkita <117151719+Njkita@users.noreply.github.com>
2024-04-25 07:59:32 +0300

    Update Main.kt

--- 2_zadanie/Main.kt
+++ 2_zadanie/Main.kt
@@ -6,12 +6,12 @@ fun main() {
     //любая ссылка
     val url = "https://ru.wikipedia.org/wiki/%D0%9F%D1%81%D0%B5%D0%B2%D0%B4%D0%BE%D1%8D%D1%84%D0%B5%D0%B4%D1%80%D0%B8%D0%BD"
     val links = Geturl(url)
-    for (link in links) {
-        println(link)
-    }
+    //for (link in links) {
+    println(links)
+    //}
 }
 
-fun Geturl (Stringurl: String): List<Char> {
+fun Geturl (Stringurl: String): String {
     val url = URL(Stringurl)
     val compound = url.openConnection()
     val allline = StringBuilder()
@@ -21,5 +21,5 @@ fun Geturl (Stringurl: String): List<Char> {
             allline.append(line)
         }
     }
-    //return allline.toList()
+    return allline.toString()
 }
