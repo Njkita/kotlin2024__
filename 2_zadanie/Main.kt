fad18353cb25053c7726e9cbca2736e6db00ee34
Njkita <117151719+Njkita@users.noreply.github.com>
2024-04-25 07:48:35 +0300

    Update Main.kt

--- 2_zadanie/Main.kt
+++ 2_zadanie/Main.kt
@@ -1,3 +1,25 @@
-fun main() {
+import java.net.URL
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
+fun main() {
+    //любая ссылка
+    val url = "https://ru.wikipedia.org/wiki/%D0%9F%D1%81%D0%B5%D0%B2%D0%B4%D0%BE%D1%8D%D1%84%D0%B5%D0%B4%D1%80%D0%B8%D0%BD"
+    val links = Geturl(url)
+    for (link in links) {
+        println(link)
+    }
+}
+
+fun Geturl (Stringurl: String): List<Char> {
+    val url = URL(Stringurl)
+    val compound = url.openConnection()
+    val allline = StringBuilder()
+    BufferedReader(InputStreamReader(compound.getInputStream())).use { reader ->
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+            allline.append(line)
+        }
+    }
+    //return allline.toList()
 }
