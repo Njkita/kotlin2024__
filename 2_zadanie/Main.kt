10ba9dd0d3e30587e54ac80dddf601f0d0673246
Njkita <117151719+Njkita@users.noreply.github.com>
2024-04-25 22:25:09 +0300

    Update Main.kt

--- 2_zadanie/Main.kt
+++ 2_zadanie/Main.kt
@@ -2,24 +2,37 @@ import java.net.URL
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
-fun main() {
-    //любая ссылка
-    val url = "https://ru.wikipedia.org/wiki/%D0%9F%D1%81%D0%B5%D0%B2%D0%B4%D0%BE%D1%8D%D1%84%D0%B5%D0%B4%D1%80%D0%B8%D0%BD"
-    val text = Geturl(url)
-    //for (link in text) {
-    println(text)
-    //}
-}
-
 fun Geturl (Stringurl: String): String {
     val url = URL(Stringurl)
     val compound = url.openConnection()
     val allline = StringBuilder()
+
     BufferedReader(InputStreamReader(compound.getInputStream())).use { reader ->
         var line: String?
         while (reader.readLine().also { line = it } != null) {
             allline.append(line)
         }
     }
+
     return allline.toString()
+}
+
+fun derivelinks(html: String): List<String> {
+    //Регулярное выражение для ссылок:
+    val linkRegular = "<a\\s+(?:[^>]*?\\s+)?href=\"([^\"]*)\"".toRegex()
+    val htmllink = linkRegular.findAll(html)
+    return htmllink.map { output ->
+        val link = output.groupValues[1]
+        if (link.startsWith("#")) Unit
+        if (link.startsWith("/")) "https://en.wikipedia.org" + link else link
+    }.toList()
+}
+
+fun main() {
+    val url1 = "https://ru.wikipedia.org/wiki/%D0%9F%D1%81%D0%B5%D0%B2%D0%B4%D0%BE%D1%8D%D1%84%D0%B5%D0%B4%D1%80%D0%B8%D0%BD"
+    val text = Geturl(url1)
+    val links = derivelinks(text)
+    for (link in links) {
+        println(link)
+    }
 }
